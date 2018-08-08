fbc58e438ac820396572a9720679852acdd3b08d
phetter <joe@subproto.com>
2018-08-08 18:21:53 -0400

    main module psuedo

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// neotools main system module file
+
+
+// PSUEDO
+
+// Parse command line options
+
+// determine if interactive or cli
+
+// invoke respective module
+
+  // invoke cli
+  // invoke interactive
+  // invoke help
+  // invoke api reference
