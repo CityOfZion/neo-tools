edd4543adac9f07da95dda9e3f5d051f2007586a
phetter <joe@subproto.com>
2018-08-08 18:15:26 -0400

    skeleton build v1 psuedo

---
+++ src/cli/cli.js
@@ -0,0 +1,10 @@
+// neotools main system file
+
+
+
+
+// PSEUDO:
+
+// Parse command line using commander
+
+// Convert respective module io to stdio. I.e., take arguments from stdin or cli and pass retturn values to stdout
