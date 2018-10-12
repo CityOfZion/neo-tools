ac17318c642965baeb2f56285b42a3ad58c39046
phetter <phetter@users.noreply.github.com>
2018-10-12 16:30:40 -0400

    network.getNodesByTallest() and getNodesByLeastConnections()

--- src/nodejs/neon-js/cli/rpc/getBlockCount.js
+++ src/nodejs/neon-js/cli/rpc/getBlockCount.js
@@ -11,7 +11,6 @@ const neon      = require('@cityofzion/neon-js')
 const dbg       = require('nodejs_util/debug')
 const netutil   = require('nodejs_util/network')
 
-
 var cfg       = require('nodejs_config/config.js')
 var config    = cfg.load('nodejs_config/nodejs.config.json')
 
@@ -23,7 +22,7 @@ function print(msg) {
 }
 
 program
-  .version('0.1.0')
+  .version('0.2.0')
   .usage('')
   .option('-d, --debug', 'Debug')
   .option('-n, --node [node]', 'set RPC node to use (be sure to preface with https://)')
@@ -41,10 +40,13 @@ if (!program.node) {
   // get a node from the list and try it
   let net = netutil.resolveNetworkId(program.Net)
 
-  print('net: '+program.Net)
-  print('net: '+net)
+  let nodes = cfg.get_nodes(net)
 
-  dbg.logDeep('nets: ', cfg.get_nodes(net))
+  if (defly) dbg.logDeep('config nodes: ', nodes)
+
+  netutil.getNodesByTallest(nodes).then(rankedNodes => {
+    if (defly) dbg.logDeep('sorted nodes: ', rankedNodes)
+  })
 
 } else node = program.node
 
--- src/nodejs/util/network.js
+++ src/nodejs/util/network.js
@@ -4,8 +4,10 @@
 
 require('module-alias/register')
 
+const _       = require('underscore')
 
 const dbg     = require('nodejs_util/debug')
+var neon      = require('@cityofzion/neon-js')
 
 var cfg       = require('nodejs_config/config.js')
 var config    = cfg.load('nodejs_config/nodejs.config.json')
@@ -28,4 +30,61 @@ exports.resolveNetworkId = (networkId) => {
   else net = networkId
 
   return net
+}
+
+
+// Return an array of nodes sorted into a list with tallest first
+// This expects an array of nodes with keys named "url"
+// TODO: add caching with configurable time limit for results
+
+exports.getNodesByTallest = (nodes) => {
+  let rankedList = []
+  let i = 0
+  return new Promise((resolve, reject) => {
+
+    if (_.isArray(nodes)) {
+      nodes.forEach((n) => {
+        if (n.url) {
+          const client = neon.default.create.rpcClient(n.url)
+
+          client.getBlockCount().then(response => {
+            rankedList.push({ "url": n.url, "height": response })
+
+            if (i++ === (nodes.length - 1)) {
+              rankedList = _.sortBy(rankedList, 'height')
+              rankedList = rankedList.reverse()
+              resolve(rankedList)
+            }
+          })
+        }
+      })
+    }
+  })
+}
+
+// Return an array of nodes sortyed with least connections first
+// This expects an array of nodes with keys named "url"
+// TODO: add caching with configurable time limit for results
+
+exports.getNodesByLeastConnections = (nodes) => {
+  let rankedList = []
+  let i = 0
+
+  return new Promise((resolve, reject) => {
+    if (_.isArray(nodes)) {
+      nodes.forEach((n) => {
+        if (n.url) {
+          const client = neon.default.create.rpcClient(n.url)
+
+          client.getConnectionCount().then(response => {
+            rankedList.push({ "url": n.url, "connections": response })
+            if (i++ === (nodes.length - 1)) {
+              rankedList = _.sortBy(rankedList, 'connections')
+              resolve(rankedList)
+            }
+          })
+        }
+      })
+    }
+  })
 }
