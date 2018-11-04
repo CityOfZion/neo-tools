4b4ab69ce80bbd328cbd3ee0aae1f806d5b6272e
phetter <phetter@users.noreply.github.com>
2018-11-04 12:14:40 -0500

    neo-rpc.js cli template

--- src/nodejs/neo-rpc/v2.9.0/client/cli/neo-rpc.js
+++ src/nodejs/neo-rpc/v2.9.0/client/cli/neo-rpc.js
@@ -20,34 +20,41 @@ let arg
 function print(msg) {
   console.log(msg);
 }
-//
-// program
-//   .version('0.2.0')
-//   .usage('')
-//   .usage('')
-//   .option('-d, --debug', 'Debug')
-//   .option('-n, --node [node]', 'set RPC node to use (be sure to preface with https://), if not provided will try to use node with tallest block')
-//   .option('-h, --hash [hash]', 'specify the hash of the block to fetch, if no hash or index is supplied will get the tallest')
-//   .option('-i, --index [index]', 'specify the number of the block to fetch, if no hash or index is supplied will get the tallest')
-//   .option('-t, --time', 'Only return time field of last block - this does not work with -T option')
-//   .option('-T, --Txs', 'Only return an array of transactions for the block')
-//   .option('-H, --Human', 'I am human so make outputs easy for human')
-//   .option('-N, --Net [Net]', 'Select network [net]: i.e., TestNet or MainNet', 'TestNet')
-//   .on('--help', function(){
-//     print('OPTIMIZATION NOTE: \n\nAs of /NEO:2.8.0/, the only difference in the return value of getRawTransaction versus getBlock is three fields more in the former: blockhash, confirmations, and blocktime. Don\'t make the extra RPC call to getRawTransaction if you don\'t need to.')
-//   })
-//   .parse(process.argv)
 
-netutil.debug()
+program
+  .version('0.2.0')
+  .usage('')
+  .usage('')
+  .option('-d, --debug', 'Debug')
+  .option('-n, --node [node]', 'set RPC node to use (be sure to preface with https://), if not provided will try to use node with tallest block')
+  .option('-t, --time', 'Only return time field of last block - this does not work with -T option')
+  .option('-T, --Txs', 'Only return an array of transactions for the block')
+  .option('-H, --Human', 'I am human so make outputs easy for human')
+  .option('-N, --Net [Net]', 'Select network [net]: i.e., TestNet or MainNet', 'TestNet')
+  .option('-m, --method [method]', 'Call node with this RPC method, default \'getBlockCount\'', 'getBlockCount')
+  .option('-p, --params [params]', 'Call RPC method with these params, default is blank', '')
+  .on('--help', function(){
+    print('OPTIMIZATION NOTE: \n\nAs of /NEO:2.8.0/, the only difference in the return value of getRawTransaction versus getBlock is three fields more in the former: blockhash, confirmations, and blocktime. Don\'t make the extra RPC call to getRawTransaction if you don\'t need to.')
+  })
+  .parse(process.argv)
 
+if (program.hash) arg = program.hash
+if (program.index) arg = parseInt(program.index)
 
-let options = {
-  net: 'main',
+if (!program.node) {
+  let options = {
+    net: program.net,
+  }
+  netutil.getNodesByPing(options).then(rankedNodes => {
+    if (defly) dbg.logDeep(__filename + ': getNodesByPing().rankedNodes: ', rankedNodes)
+    nodes = rankedNodes
+    commandWrapper(nodes)
+  })
+  .catch (error => {
+    print(__filename + ': ' + error.message)
+  })
 }
-netutil.getNodesByPing(options).then(rankedNodes => {
-  if (defly) dbg.logDeep(__filename + ': getNodesByPing().rankedNodes: ', rankedNodes)
-  dbg.logDeep(' ', rankedNodes)
-})
-.catch (error => {
-  print(__filename + ': ' + error.message)
-})
+else {
+  nodes.push({ "url": program.node })
+  commandWrapper(nodes)
+}
